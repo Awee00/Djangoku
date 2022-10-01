6c2b901ab60309a96b34cb713752386e08f07deb
Wahyu Awee <ardawardana70@gmail.com>
2022-10-01 20:49:53 +0700

    Tutor kedua

---
+++ mysite/polls/models.py
@@ -0,0 +1,20 @@
+from email.policy import default
+from secrets import choice
+from statistics import mode
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self) -> str:
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self) -> str:
+        return self.choice_text
+
+# Create your models here.
